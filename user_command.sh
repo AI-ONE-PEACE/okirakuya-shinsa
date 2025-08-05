a44f14fab783e92f0b57cf66682c1a8a715ecd02
AI-ONE-PEACE <norikazu.ieda@gmail.com>
2025-08-06 00:51:20 +0900

    make it better

--- user_command.sh
+++ user_command.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# bundle install を実行
+# Gemをプロジェクト内の 'vendor/bundle' ディレクトリにインストールします
 bundle install --deployment --path vendor/bundle --without development test
 
-# データベースのマイグレーションを実行
+# データベースのマイグレーションを実行します
 bundle exec rails db:migrate
 
-# アセットのプリコンパイルを実行
+# アセットのプリコンパイルを実行します
 bundle exec rails assets:precompile
+
+# Webサーバーを再起動します
+touch tmp/restart.txt
