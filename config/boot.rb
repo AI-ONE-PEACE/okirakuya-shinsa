27a5315454560ade0dcd3f09a0e5d26b79fa4543
AI-ONE-PEACE <norikazu.ieda@gmail.com>
2025-08-12 00:04:19 +0900

    モデルを実装

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,6 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+pid_file = File.expand_path("../tmp/pids/server.pid", __dir__)
+File.delete(pid_file) if File.exist?(pid_file)
